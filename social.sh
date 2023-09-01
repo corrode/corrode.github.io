7ae5ebf0676a7db2a3822764862cbd43a7a521e2
Matthias <matthias-endler@gmx.net>
2023-09-01 15:45:36 +0200

    Update comment

--- social.sh
+++ social.sh
@@ -16,7 +16,7 @@ generate_social_image() {
 
 # Iterate over blog posts
 for post in content/blog/*.md; do
-    # Check if the file starts with an underscore or is .gitignore
+    # Skip files, which start with an underscore
     if [[ $(basename "$post") == _* ]]; then
         continue
     fi
