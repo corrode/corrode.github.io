3f1bae402d316863d2ebd34f85b76ae6946f1250
Matthias <matthias@endler.dev>
2025-01-28 17:18:16 +0100

    Update social images

--- scripts/social.sh
+++ scripts/social.sh
@@ -4,22 +4,36 @@ set -euo pipefail
 
 # Function to generate social image for a given title
 generate_social_image() {
-    local title="$1"
+    local template_file=$1
     local output_file="$2"
-    
-    # Generate the caption image
-    magick -background none -fill '#000000' -font Inter-Bold -pointsize 80 -size 670x caption:"$title" text.png
 
-    # Composite the caption over the background image
-    magick static/social/social-post-template.svg text.png -gravity northwest -geometry +80+80 -composite "$output_file"
+    # Title can be unbound
+    local title=${3:-}
+
+    echo "Generating social image with title >$title< at $output_file"
+
+    if [[ -z "$title" ]]; then
+        # No caption; copy the template file as is
+        cp "$template_file" "$output_file"
+    else
+        # Generate the caption image
+        magick -background none -fill '#000000' -font Inter-Bold -pointsize 80 -size 670x caption:"$title" text.png
+
+        # Composite the caption over the background image
+        magick "$template_file" text.png -gravity northwest -geometry +80+80 -composite "$output_file"
+    fi
 }
 
 process_post() {
     local post="$1"
     local post_filename="$2"
+    local parent_dir="$3"
+
+    local title 
+    local output_path
     
     # Extract the title from the frontmatter
-    title=$(awk -F= '/^title[[:space:]]*=/ {gsub(/"/, "", $2); print $2}' "$post")
+    title=$(awk -F'"' '/^title/ {print $2}' "$post")
 
     # Check if the title is empty
     if [[ -z "$title" ]]; then
@@ -27,8 +41,16 @@ process_post() {
         return
     fi
 
-    # Generate the output file path
-    output_path="static/social/rendered/${post_filename}.png"
+    # Put the social image into the same directory as the post 
+    output_path="${parent_dir}/social.png"
+    
+    # Use the podcast template if the post is in a podcast directory 
+    # The title is empty as the image already contains text 
+    if [[ "$parent_dir" == *"/podcast"* ]]; then
+        template_file="static/social/podcast.png"
+        generate_social_image "$template_file" "$output_path"
+        return
+    fi
     
     if [[ -f "$output_path" ]]; then
         echo "File already exists: $output_path. Skipping."
@@ -37,37 +59,47 @@ process_post() {
     
     echo "Generating $title ($output_path)"
 
-    # Call the function to generate the social image
-    generate_social_image "$title" "$output_path"
+    local template_file="static/social/default-template.svg"
+    generate_social_image "$template_file"  "$output_path" "$title" 
 }
 
-# Iterate over blog posts and folders
-for item in content/blog/*; do
-    # Skip files, which start with an underscore
-    if [[ $(basename "$item") == _* ]]; then
-        continue
-    fi
+process_directory() {
+    local dir="$1"
+    local base_output_name="$2"
 
-    if [[ -d "$item" && -f "$item/index.md" ]]; then
-        # Process the index.md inside the folder
-        process_post "$item/index.md" "$(basename "$item")"
-    elif [[ -f "$item" ]]; then
-        # Process the individual markdown file
-        process_post "$item" "$(basename "$item" .md)"
+    echo "Processing directory: $dir, base_output_name: $base_output_name" 
+    
+    # Process _index.md if it exists
+    if [[ -f "$dir/_index.md" ]]; then
+        process_post "$dir/_index.md" "${base_output_name}" "$dir"
     fi
-done
-
-# Iterate over other folders in content
-for item in content/*; do
-    # Skip files, which start with an underscore
-    if [[ $(basename "$item") == _* ]]; then
-        continue
+    
+    # Process regular index.md if it exists
+    if [[ -f "$dir/index.md" ]]; then
+        process_post "$dir/index.md" "${base_output_name}" "$dir"
     fi
+    
+    # Recursively Process all subdirectories
+    local subdir_basename
+    for subdir in "$dir"/*; do
+        if [[ -d "$subdir" && $(basename "$subdir") != _* ]]; then
+            subdir_basename="${base_output_name}-$(basename "$subdir")"
+            process_directory "$subdir" "$subdir_basename"
+        fi
+    done
+    
+    # Process markdown files in current directory
+    for file in "$dir"/*.md; do
+        if [[ -f "$file" && $(basename "$file") != _* && $(basename "$file") != "index.md" ]]; then
+            process_post "$file" "${base_output_name}-$(basename "$file" .md)" "$dir"
+        fi
+    done
+}
 
-    # Include folders, which have an _index.md file
-    if [[ -d "$item" && -f "$item/_index.md" ]]; then
-        # Process the index.md inside the folder
-        process_post "$item/_index.md" "$(basename "$item")"
+# Process all content directories
+for content_dir in content/*; do
+    if [[ -d "$content_dir" && $(basename "$content_dir") != _* ]]; then
+        process_directory "$content_dir" "$(basename "$content_dir")"
     fi
 done
 
